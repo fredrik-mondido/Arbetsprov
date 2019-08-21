4d4cf2df4e9e66709d56e39eadd5f72f76136c6f
Storchillarn <gurr_gurr@hotmail.com>
2019-08-21 21:55:11 +0200

    refactor TodosView

--- src/views/TodosView.js
+++ src/views/TodosView.js
@@ -4,14 +4,26 @@ import List from '../components/list';
 
 const TodosView = ({ todos, addTodoSubmitHandler, setActiveTodo }) => {
 
+    const columns = ['created', 'active', 'completed', 'onHold'];
+    const formatTitle = name => {
+        const capitalchar = name.match(/[A-Z]/);
+        const firstChar = name.charAt(0).toUpperCase();
+        name = capitalchar ? name.replace(capitalchar, ` ${capitalchar}`) : name;
+        return firstChar + name.slice(1);
+    }
+
     return (
         <div>
             <TodoForm title="Create Todo" submitHandler={addTodoSubmitHandler} />
             <div className="list-container">
-                <List todos={todos} column="created" title="Created" setActiveTodo={setActiveTodo} />
-                <List todos={todos} column="active" title="Active" setActiveTodo={setActiveTodo} />
-                <List todos={todos} column="completed" title="Completed" setActiveTodo={setActiveTodo} />
-                <List todos={todos} column="onHold" title="On hold" setActiveTodo={setActiveTodo} />
+                {columns.map((name, i) =>
+                    <List key={i}
+                        todos={todos}
+                        column={name}
+                        title={formatTitle(name)}
+                        setActiveTodo={setActiveTodo}
+                    />
+                )}
             </div>
         </div>
     )
