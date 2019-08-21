3efe44defdd469bbffcd4f43f23b4da9ccf1b257
Storchillarn <gurr_gurr@hotmail.com>
2019-08-21 09:51:49 +0200

    create basic todo-form

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import List from '../list';
+import TodoForm from '../todo-form';
 
 const App = () => {
     return (
         <div>
+            <TodoForm title="Create Todo" />
             <List />
         </div>
     )
